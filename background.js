57d3dffc283b2180c3d9030ab8d7b8e4a66e185d
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-06-11 20:02:57 -0400

    Add files via upload

---
+++ background.js
@@ -0,0 +1,21 @@
+
+// sheesh
+function sheesh() {
+  const sheesh = chance();
+
+  if (sheesh) {
+    const body = document.body;
+    body.innerHTML = "<video autoplay loop style='height: 100%; width: 100%'><source src='https://raw.githubusercontent.com/someStranger8/Sheesh/master/dh.mp4' type='video/mp4'></video>";
+  }
+}
+
+// chance
+function chance() {
+  const chance = Math.floor(Math.random() * 100);
+
+  if (chance == 69) {
+    return true;
+  }
+
+  return false;
+}
